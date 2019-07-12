cb3fe2ddae0afb619e1c5652d89c888ff005a5db
kimjunhee <developer9101@gmail.com>
2019-07-12 10:48:22 +0900

    weather component 추가, 기본 디자인

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import Weather from './weather.js';
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: true
   }
 
   render() {
     const { isLoaded } = this.state
     return (
       <View style={styles.container}>
-        {isLoaded ? null : (
+        {isLoaded ? <Weather /> : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
           </View>
